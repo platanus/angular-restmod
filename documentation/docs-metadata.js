2f67e92ffc5259ea949fb7659db51a58c58c19f9
Ignacio Baixas <iobaixas@gmail.com>
2013-10-27 20:37:27 -0300

    Updates

---
+++ documentation/docs-metadata.js
@@ -0,0 +1,71 @@
+DOC_DATA=[
+{"section":"api","group":"restmod","id":"restmod.service:$restmod","name":false,"shortName":"$restmod","docType":"service","module":"restmod","moduleSection":"service","moduleItem":"$restmod","moduleSubItem":"","keywords":"about abstract an be building by can chain class container dsl factory for generate generation in included information is just metadata method mixin mode model modelbuilder more provided restmod returns rich see service that the to types used using","codeBlocks":[
+{"lineStart":74,"lineEnd":83},
+{"lineStart":1218,"lineEnd":1227}],"source":false},
+{"section":"api","group":"restmod","id":"restmod.service:$restmodProvider","name":false,"shortName":"$restmodProvider","docType":"service","module":"restmod","moduleSection":"service","moduleItem":"$restmodProvider","moduleSubItem":"","keywords":"$provider $restmod $restmodprovider abstract added adds all are base be by chain changemodel configuration default empty every exposes generated is javascript lazyrelations method methods mixins model models non not prepended pushmodelbase restmod service should the this throttledmodel to usage","codeBlocks":[
+{"lineStart":37,"lineEnd":43},
+{"lineStart":50,"lineEnd":68}],"source":false},
+{"section":"api","group":"restmod","id":"index","name":false,"shortName":"API Reference","docType":"overview","module":"index","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"all api are base building class collection collections components docs dsl for generated implementation index main model modelbuilder models overview reference restmod the to welcome","codeBlocks":[
+{"lineStart":1,"lineEnd":15}],"source":false},
+{"section":"guides","group":"restmod","id":"model","name":false,"shortName":"Basic model usage","docType":"overview","module":"model","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"basic chain mixin model overview the usage","codeBlocks":[
+{"lineStart":1,"lineEnd":6}],"source":false},
+{"section":"guides","group":"restmod","id":"building","name":false,"shortName":"Building a Model","docType":"overview","module":"building","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"building chain mixin model overview the","codeBlocks":[
+{"lineStart":1,"lineEnd":6}],"source":false},
+{"section":"api","group":"restmod","id":"restmod.class:Collection","name":false,"shortName":"Collection","docType":"interface","module":"restmod","moduleSection":"class","moduleItem":"Collection","moduleSubItem":"","keywords":"$build $buildraw $collection $create $feed $fetch $find $reset $resolved $search $then $url _attr _context _error _init _opt _params _raw _success _url add additional also an and api are as attempts be begin bound builder builds callback calls chain chaining class col collection collections contents context corresponding data error feeds for generated generates given if in initial instance instances interface into is it its keeps key marks method model new not object of on only optional options override parameters passed populate post primary promise provide provided queries query raw request resets resolve resource restmod returns saved saves scenarios self server sets stored string success support the then these this to todo treated undecoded url usage use using values when","codeBlocks":[
+{"lineStart":420,"lineEnd":427},
+{"lineStart":430,"lineEnd":438},
+{"lineStart":444,"lineEnd":454},
+{"lineStart":470,"lineEnd":478},
+{"lineStart":484,"lineEnd":492},
+{"lineStart":498,"lineEnd":508},
+{"lineStart":523,"lineEnd":535},
+{"lineStart":559,"lineEnd":567},
+{"lineStart":575,"lineEnd":588},
+{"lineStart":596,"lineEnd":605},
+{"lineStart":614,"lineEnd":624},
+{"lineStart":633,"lineEnd":645}],"source":false},
+{"section":"guides","group":"restmod","id":"index","name":false,"shortName":"Developer Guides","docType":"overview","module":"index","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"developer group guides index overview the to welcome","codeBlocks":[
+{"lineStart":1,"lineEnd":8}],"source":false},
+{"section":"api","group":"restmod","id":"restmod.class:Model","name":false,"shortName":"Model","docType":"interface","module":"restmod","moduleSection":"class","moduleItem":"Model","moduleSubItem":"","keywords":"$callback $context $decode $destroy $encode $error $fetch $http $partial $pending $promise $restmod $save $send $then $url _action _error _hook _opt _options _raw _success action actions additional all allows an any api are arguments as available base be begin bound builder by callback calling chain chaining check class col collection communication construction context create creating creation custom data destroy direct documentation error every fed feed fetch for from generate generated go hook hooks if implementing information inherits instance instances interface is keeps last level low method methods model modelbuilder more name new not object of on options originated partial passed passes point promise property provided raw recommended registered relative render request resource restmod returns self sent server starting static status success sucess that the this through to type updated url usage usefull using when wraps","codeBlocks":[
+{"lineStart":150,"lineEnd":170},
+{"lineStart":196,"lineEnd":204},
+{"lineStart":210,"lineEnd":221},
+{"lineStart":228,"lineEnd":238},
+{"lineStart":245,"lineEnd":256},
+{"lineStart":263,"lineEnd":272},
+{"lineStart":295,"lineEnd":303},
+{"lineStart":322,"lineEnd":331},
+{"lineStart":345,"lineEnd":354},
+{"lineStart":398,"lineEnd":407}],"source":false},
+{"section":"guides","group":"restmod","id":"relations","name":false,"shortName":"Model Relations","docType":"overview","module":"relations","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"chain mixin model overview relations the","codeBlocks":[
+{"lineStart":1,"lineEnd":6}],"source":false},
+{"section":"api","group":"restmod","id":"restmod.class:ModelBuilder","name":false,"shortName":"ModelBuilder","docType":"interface","module":"restmod","moduleSection":"class","moduleItem":"ModelBuilder","moduleSubItem":"","keywords":"$fetch $super _attr _chain _description _do _expr _factory _filter _filterparam _fun _hook _init _isvolatile _mapping _mask _model _name _param1 _reset _serializer _test _url _value above accross added adds after alltogether also alternatively an and angular any api are argument as assigns attrdecoder attrdefault attrencoder attrexpression attribute attributes attrignored attrserializer attrvolatile available baseurl be behavior being both builder building builtin by call callbacks called calling calls camelcase can case chained change changes chapter class classdefine consistent construction context createdat current data date de decode decoded decoding defaulf default defaults define defines definition depending describe description descriptions designed different direct disabled disablerenaming disables docs dsl each encode encoded encoding equivalent evaluate evaluated event every example executed existing expression extend extends factory false fed filter following for function generator get given hasmany hasone hello hook hooks if ignore ignores implement in iniline injectable injector inside instance intended interface is it its js json keeping keyword languajes later link listening local make mapping maps mask masmask matches merge method methods misc model modifiers modify name naming new not object of old on only or other override overriden overview param param2 parameter parameters parses partial passed phase posible proper properties property provide provided provides receives refer reference register registers relation renames renaming rendered represents required reset resource restmod return returns same self sent serializer server set setnamedecoder setnameencoder sets seturlbuilderfactory should snake string style supports system test testattr testp1 testp2 that the then this time to todo true type underscores url usage use used uses using value values various volatile way wharever when will with works world yy-mm-dd","codeBlocks":[
+{"lineStart":690,"lineEnd":696},
+{"lineStart":703,"lineEnd":716},
+{"lineStart":723,"lineEnd":737},
+{"lineStart":744,"lineEnd":758},
+{"lineStart":765,"lineEnd":772},
+{"lineStart":780,"lineEnd":809},
+{"lineStart":821,"lineEnd":830},
+{"lineStart":841,"lineEnd":861},
+{"lineStart":881,"lineEnd":895},
+{"lineStart":903,"lineEnd":915},
+{"lineStart":932,"lineEnd":946},
+{"lineStart":960,"lineEnd":971},
+{"lineStart":984,"lineEnd":995},
+{"lineStart":1008,"lineEnd":1021},
+{"lineStart":1032,"lineEnd":1045},
+{"lineStart":1056,"lineEnd":1074},
+{"lineStart":1085,"lineEnd":1098},
+{"lineStart":1109,"lineEnd":1124},
+{"lineStart":1146,"lineEnd":1155},
+{"lineStart":1164,"lineEnd":1173}],"source":false},
+{"section":"guides","group":"restmod","id":"urlbuilder","name":false,"shortName":"The Url Builder","docType":"overview","module":"urlbuilder","moduleSection":"","moduleItem":"","moduleSubItem":"","keywords":"builder chain mixin overview the url urlbuilder","codeBlocks":[
+{"lineStart":1,"lineEnd":6}],"source":false},
+{"section":"api","group":"restmod","id":"restmod.global:Utils","name":false,"shortName":"Utils","docType":"interface","module":"restmod","moduleSection":"global","moduleItem":"Utils","moduleSubItem":"","keywords":"$super _first _fun _other _sep _string _super _target across an as available be by call camelcase camelcased case chain chains defaults diacritics extend extended extendoverriden filtering function functions global handle instead interface it just last library making method new object of on original override overriden replacing representation restmod result returned separator snakecase source string the this to todo together transforms used using utilities utils value various","codeBlocks":[
+{"lineStart":3,"lineEnd":7},
+{"lineStart":10,"lineEnd":20},
+{"lineStart":28,"lineEnd":39},
+{"lineStart":47,"lineEnd":56},
+{"lineStart":64,"lineEnd":73},
+{"lineStart":88,"lineEnd":96}],"source":false}];
---
+++ documentation/groups-metadata.js
@@ -0,0 +1,5 @@
+GROUP_DATA=[
+{"groupTitle":"Resources","groupId":"restmod","groupIcon":"icon-book","sections":[
+{"id":"api","title":"Restmod API","showSource":false,"scripts":["src/","src/plugins/"],"docs":["docs/api/"],"doc_api":"angular"},
+{"id":"guides","title":"Guides","docs":["docs/guides/"],"rank":
+{"building":1,"model":2,"relations":3,"urlbuilder":4},"doc_api":"angular"}],"visible":true}];
