e792d1644a9693aac6b2d06d93f30bca71cea8d3
Ignacio Baixas <iobaixas@gmail.com>
2013-10-31 17:31:49 -0300

    Updates

--- documentation/docular-configuration.js
+++ documentation/docular-configuration.js
@@ -1,1 +1,1 @@
-baseURL = '/angular-restmod/'; addTag('base', {href: '/angular-restmod/'});  window.discussionConfigs = {active:false, shortName:'false', url:'false', dev:false}; 
+baseURL = '/angular-restmod'; addTag('base', {href: '/angular-restmod'});  window.discussionConfigs = {active:false, shortName:'false', url:'false', dev:false}; 
