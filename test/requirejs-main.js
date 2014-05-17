4019178f44a8ebc87e241b7afb412c2d77d88bee
Ignacio Baixas <iobaixas@gmail.com>
2014-05-16 22:53:19 -0400

    chore: setups karma and requirejs to include README.md file in tests

---
+++ test/requirejs-main.js
@@ -0,0 +1,29 @@
+var tests = [];
+for (var file in window.__karma__.files) {
+    if(window.__karma__.files.hasOwnProperty(file)) {
+        if(/_spec\.js$/.test(file)) {
+            tests.push(file);
+        }
+    }
+}
+
+requirejs.config({
+    // Karma serves files from '/base'
+    baseUrl: '/base',
+
+    paths: {
+        'text': './node_modules/text/text' // include text plugin!
+    },
+
+    shim: {
+        // 'underscore': {
+        //     exports: '_'
+        // }
+    },
+
+    // ask Require.js to load these files (all our tests)
+    deps: tests,
+
+    // start test run, once Require.js is done
+    callback: window.__karma__.start
+});
