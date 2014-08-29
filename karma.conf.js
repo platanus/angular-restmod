32c3946a671029ba081e8f5a67fe2fc6bbd0b808
Ignacio Baixas <iobaixas@gmail.com>
2014-08-29 19:00:35 -0400

    chore(): add sauce tests configs and travis integration

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,18 @@
+var FULL_BROWSER_TEST = process.env.TRAVIS_SECURE_ENV_VARS === 'true';
+
 // Karma configuration
 // Generated on Fri Aug 09 2013 14:14:35 GMT-0500 (CDT)
 
 module.exports = function(config) {
   config.set({
 
-    // base path, that will be used to resolve files and exclude
+    // Base path, that will be used to resolve files and exclude
     basePath: '',
 
+    // Load jasmine and requirejs (require is used by the readme spec)
     frameworks: ['jasmine', 'requirejs'],
 
-    // list of files / patterns to load in the browser
+    // List of files / patterns to load in the browser
     files: [
 		// libraries
 		'bower_components/angular/angular.js',
@@ -29,46 +32,94 @@ module.exports = function(config) {
         { pattern: 'node_modules/text/text.js', included: false }
     ],
 
-    // karma plugins
+    // Karma plugins
     plugins: [
       'karma-jasmine',
       'karma-requirejs',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
+      'karma-sauce-launcher',
       'karma-osx-reporter'
     ],
 
-    // list of files to exclude
+    // List of files to exclude
     exclude: [
 
     ],
 
-
-    // test results reporter to use
+    // Test results reporter to use
     // possible values: 'dots', 'progress', 'junit'
-    reporters: ['progress', 'osx'],
+    reporters: ['progress', 'osx', 'saucelabs'],
 
-
-    // web server port
+    // Web server port
     port: 9876,
-
 
     // cli runner port
     runnerPort: 9100,
 
-
-    // enable / disable colors in the output (reporters and logs)
+    // Enable / disable colors in the output (reporters and logs)
     colors: true,
 
-
-    // level of logging
+    // Level of logging
     // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
-    // enable / disable watching file and executing tests whenever any file changes
+    // Enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+    // Additional no activity timeout to work with external browsers
+    browserNoActivityTimeout: 60000,
+
+    // Continuous Integration mode by default
+    singleRun: false,
+
+    // Sauce config, requires username and accessKey to be loaded in ENV
+    sauceLabs: {
+        testName: 'Restmod Unit Tests',
+        startConnect: false
+    },
+
+    // Custom sauce launchers
+    customLaunchers:
+    {
+        sl_chrome: {
+          base: 'SauceLabs',
+          browserName: 'chrome',
+          platform: 'Windows 7',
+          version: '35'
+        },
+        sl_firefox: {
+          base: 'SauceLabs',
+          browserName: 'firefox',
+          version: '30'
+        },
+        sl_firefox_4: {
+          base: 'SauceLabs',
+          browserName: 'firefox',
+          version: '4'
+        },
+        sl_ios_safari: {
+          base: 'SauceLabs',
+          browserName: 'iphone',
+          platform: 'OS X 10.9',
+          version: '7.1'
+        },
+        sl_ie_9: {
+          base: 'SauceLabs',
+          browserName: 'internet explorer',
+          platform: 'Windows 7',
+          version: '9'
+        },
+        sl_ie_11: {
+          base: 'SauceLabs',
+          browserName: 'internet explorer',
+          platform: 'Windows 8.1',
+          version: '11'
+        }
+    },
 
     // Start these browsers, currently available:
     // - Chrome
@@ -78,16 +129,6 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
-
-
-    // If browser does not capture in given timeout [ms], kill it
-    captureTimeout: 60000,
-
-
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-
+    browsers: FULL_BROWSER_TEST ? ['sl_ie_9', 'sl_firefox_4', 'sl_firefox', 'sl_ie_11'] : ['Chrome']
   });
 };
