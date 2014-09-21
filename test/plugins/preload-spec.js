58c9cb096dc3e6b184341e75fd5beeffffd9711e
Ignacio Baixas <iobaixas@gmail.com>
2014-09-21 00:58:57 -0300

    feat(plugins): Adds the Preload plugin.
    The Preload plugin adds the $preload method that enables relation eager loading.
    
    Closes #75

---
+++ test/plugins/preload-spec.js
@@ -0,0 +1,101 @@
+'use strict';
+
+describe('Plugin: Preload function', function() {
+
+  var Bike, User, Ride, $q, $rootScope, $httpBackend;
+
+  beforeEach(module('restmod'));
+
+  beforeEach(inject(function(restmod, $injector) {
+
+    Ride = restmod.model('/api/rides');
+
+    User = restmod.model('/api/users');
+    User.mix({
+      rides: { belongsToMany: Ride },
+      friends: { belongsToMany: User }
+    });
+
+    Bike = restmod.model('/api/bikes').mix('restmod.Preload', {
+      user: { belongsTo: User }
+    });
+
+    $q = $injector.get('$q');
+    $rootScope = $injector.get('$rootScope');
+    $httpBackend = $injector.get('$httpBackend');
+  }));
+
+  describe('$preload', function() {
+
+    var bikes;
+
+    beforeEach(function() {
+      bikes = Bike.$collection().$decode([
+        { userId: 10, brand: 'Santa Cruz' },
+        { userId: 11, brand: 'BMC' },
+        { userId: 13, brand: 'Iron Horse' }
+      ]);
+    });
+
+    it('should call populate on preloaded resource ', function() {
+      User.$populate = jasmine.createSpy().andReturn(User.dummy(true));
+
+      bikes.$preload('user');
+      expect(User.$populate).toHaveBeenCalledWith([
+        jasmine.objectContaining({ $pk: 10 }),
+        jasmine.objectContaining({ $pk: 11 }),
+        jasmine.objectContaining({ $pk: 13 })
+      ]);
+      expect(User.$populate.callCount).toEqual(1);
+    });
+
+    it('should work on single records too', function() {
+      var bike = Bike.$new(1).$decode({ userId: 10, brand: 'Santa Cruz' });
+      User.$populate = jasmine.createSpy().andReturn(User.dummy(true));
+
+      bike.$preload('user');
+      expect(User.$populate).toHaveBeenCalledWith([ jasmine.objectContaining({ $pk: 10 }) ]);
+    });
+
+    it('should properly manager hierachies', function() {
+      User.$populate = jasmine.createSpy().andCallFake(function(_records) {
+        _records[0].$decode({ rideIds: [1], friendIds: [1] });
+        _records[1].$decode({ rideIds: [5], friendIds: [] });
+        _records[2].$decode({ rideIds: [5], friendIds: [2, 3] });
+        return User.dummy(true);
+      });
+
+      Ride.$populate = jasmine.createSpy().andReturn(User.dummy(true));
+
+      bikes.$preload('user.friends', 'user.rides');
+      expect(User.$populate.callCount).toEqual(1);
+      expect(Ride.$populate.callCount).toEqual(0);
+      $rootScope.$apply();
+
+      expect(Ride.$populate.callCount).toEqual(1);
+      expect(Ride.$populate).toHaveBeenCalledWith([
+        jasmine.objectContaining({ $pk: 1 }),
+        jasmine.objectContaining({ $pk: 5 }),
+        jasmine.objectContaining({ $pk: 5 })
+      ]);
+
+      expect(User.$populate.callCount).toEqual(2);
+      expect(User.$populate).toHaveBeenCalledWith([
+        jasmine.objectContaining({ $pk: 1 }),
+        jasmine.objectContaining({ $pk: 2 }),
+        jasmine.objectContaining({ $pk: 3 })
+      ]);
+
+    });
+
+    it('should fallback to $resolve if $populate is not defined', function() {
+      bikes.$preload('user');
+      $httpBackend.expectGET('/api/users/10').respond(200, {});
+      $httpBackend.expectGET('/api/users/11').respond(200, {});
+      $httpBackend.expectGET('/api/users/13').respond(200, {});
+      $httpBackend.flush();
+    });
+  });
+
+
+});
