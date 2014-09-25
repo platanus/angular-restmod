7fc1eee8bd8eaeb2cd330e1e4579ac0473e2892f
Ignacio Baixas <iobaixas@gmail.com>
2014-09-25 00:36:25 -0300

    1.1.3

--- dist/plugins/debounced.min.js
+++ dist/plugins/debounced.min.js
@@ -1,6 +1,6 @@
 /**
  * API Bound Models for AngularJS
- * @version v1.1.2 - 2014-09-24
+ * @version v1.1.3 - 2014-09-25
  * @link https://github.com/angular-platanus/restmod
  * @author Ignacio Baixas <ignacio@platan.us>
  * @license MIT License, http://www.opensource.org/licenses/MIT
